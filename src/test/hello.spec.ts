efff15e817aa1a6573e18bebd0e4ba36a864db22
sarbear28 <138033132+sarbear28@users.noreply.github.com>
2026-03-07 11:49:02 -0500

    installed java, updated hello.spec.ts to talk to the API using generated SDK client

--- src/test/hello.spec.ts
+++ src/test/hello.spec.ts
@@ -1,17 +1,17 @@
 import { expect, test } from 'vitest';
 import setup, { type ServerTestContext } from './run_server';
+import { Configuration, DefaultApi } from '../../client';
 
 setup();
 
-test<ServerTestContext>('GET /books returns 200 and an array', async ({ address }) => {
-  const response = await fetch(`${address}/books`);
-  const text = await response.text();
-//logs response
-  console.log('STATUS:', response.status);
-  console.log('BODY:', text);
+test<ServerTestContext>('GET /books returns an array using the generated client', async ({ address }) => {
+  const client = new DefaultApi(
+    new Configuration({
+      basePath: address
+    })
+  );
 
-  expect(response.status).toBe(200);
+  const response = await client.listBooks();
 
-  const body = JSON.parse(text);
-  expect(Array.isArray(body)).toBe(true);
+  expect(Array.isArray(response)).toBe(true);
 });
